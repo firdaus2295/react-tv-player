2e56b159cc01ea0d0b71e835981d2907fcaa85a5
firdaus2295 <decadekaizer@gmail.com>
2024-07-14 13:19:26 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export type MediaType = {
 
 const mediaList: MediaType[] = [
   {
-    url: "https://www.youtube.com/watch?v=SkVqJ1SGeL0",
+    url: " https://mifntechnology.github.io/siaranMy/myIPtv.m3u8",
     title: "YouTube Video Sample",
     subTitle: "Caminandes 3: Llamigos",
     preview: true,
